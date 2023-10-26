7ffc9ec5066ab3385836b58c96f78cf093f82f2a
Филипп Аталлах <fillipatalla@gmail.com>
2023-10-26 22:35:11 +0300

    правки в сетинг и в модели

--- online_shop/shop/views.py
+++ online_shop/shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'shop/index.html')
+
+
+def product_list(request):
+    return render(request, 'shop/products.html')
+
+
+def product_detail(request):
+    return render(request, 'shop/product.html')
